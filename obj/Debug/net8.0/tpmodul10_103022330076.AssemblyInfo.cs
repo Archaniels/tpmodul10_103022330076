5567369b7e8f8d6ae8076bf1a27703b326de0869
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2025-04-26 21:21:04 +0700

    first commit

--- obj/Debug/net8.0/tpmodul10_103022330076.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul10_103022330076.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul10_103022330076")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3672ba8eaef856d0ad2515d425132966664f41f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul10_103022330076")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul10_103022330076")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
